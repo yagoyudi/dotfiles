130e828b9f4cdd3ecd5ed70dff9bd1942bfeca4c
yago yudi <yagofuruta@protonmail.com>
2024-12-21 15:18:09 -0300

    Update home/.config/nvim/lua/kickstart/plugins/lspconfig.lua

--- home/private_dot_config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ home/private_dot_config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -177,32 +177,18 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
+        lua_ls = {},
+        pyright = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
-
-        lua_ls = {
-          -- cmd = {...},
-          -- filetypes = { ...},
-          -- capabilities = {},
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
-            },
-          },
+        ts_ls = {},
         },
       }
 
