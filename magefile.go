6679b09618a8fa82098ee0d391368cd14a6f5168
yago yudi <yagofuruta@protonmail.com>
2024-12-21 17:00:30 -0300

    Update magefile.go

--- magefile.go
+++ magefile.go
@@ -7,16 +7,22 @@ import (
 	"github.com/magefile/mage/sh"
 )
 
+func chezmoiApply() error {
+	return sh.RunV("chezmoi", "apply", "-v")
+}
+
+func rebuildNixos() error {
+	if err := sh.RunV("sudo", "cp", "-r", "./nixos", "/etc"); err != nil {
+		return err
+	}
+	return sh.RunV("sudo", "nixos-rebuild", "switch")
+}
+
 func Deploy() error {
-	if err := sh.RunV("chezmoi", "apply", "-v"); err != nil {
+	if err := chezmoiApply(); err != nil {
 		return err
 	}
-
-	if err := sh.RunV("sudo", "nixos-rebuild", "switch"); err != nil {
-		return err
-	}
-
-	return nil
+	return rebuildNixos()
 }
 
 func EnsureMage() error {
