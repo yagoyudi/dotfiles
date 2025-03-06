9c70395df2fd3020bee6cc7f0f3dd060b7bf4af9
yyvf <yyvf@pm.me>
2025-03-06 08:16:34 -0300

    Update home/.config/wezterm/wezterm.lua

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -70,6 +70,17 @@ config.keys = {
   { key = "K", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 5}}},
   { key = "L", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 5}}},
   { key = "d", mods = "LEADER", action=wezterm.action{CloseCurrentPane={confirm=true}}},
+
+  { key="1", mods="LEADER", action=wezterm.action{ActivateTab=0} },
+  { key="2", mods="LEADER", action=wezterm.action{ActivateTab=1} },
+  { key="3", mods="LEADER", action=wezterm.action{ActivateTab=2} },
+  { key="4", mods="LEADER", action=wezterm.action{ActivateTab=3} },
+  { key="5", mods="LEADER", action=wezterm.action{ActivateTab=4} },
+  { key="6", mods="LEADER", action=wezterm.action{ActivateTab=5} },
+  { key="7", mods="LEADER", action=wezterm.action{ActivateTab=6} },
+  { key="8", mods="LEADER", action=wezterm.action{ActivateTab=7} },
+  { key="9", mods="LEADER", action=wezterm.action{ActivateTab=8} },
+
   {
     key = "w",
     mods = "LEADER",
