10cc8f64da4328ce7c43af2736982bbcfaadf950
yyvf <yyvf@pm.me>
2025-03-07 20:44:28 -0300

    Update home/.config/qutebrowser/config.py

--- home/private_dot_config/private_qutebrowser/config.py
+++ home/private_dot_config/private_qutebrowser/config.py
@@ -3,13 +3,6 @@ c.tabs.show = "never"
 c.auto_save.session = True
 
 c.colors.webpage.preferred_color_scheme = 'dark'
-c.colors.webpage.darkmode.enabled = True
-c.colors.webpage.darkmode.algorithm = 'lightness-hsl'
-c.colors.webpage.darkmode.contrast = -.022
-c.colors.webpage.darkmode.threshold.text = 150
-c.colors.webpage.darkmode.threshold.background = 100
-c.colors.webpage.darkmode.policy.images = 'always'
-c.colors.webpage.darkmode.grayscale.images = 0.35
 
 config.load_autoconfig(False)
 
