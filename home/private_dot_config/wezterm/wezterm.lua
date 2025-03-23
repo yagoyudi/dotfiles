9d8b6c8fd47fa8ebe9f8f5b5c04da6c3c06ce2b9
yyvf <yyvf@pm.me>
2025-03-23 06:49:42 -0300

    Update home/.config/wezterm/wezterm.lua

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.font_size = 11
+config.font_size = 12
 config.font = wezterm.font("Fira Code")
 
 config. window_padding = {
