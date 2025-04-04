98d9035d31ce4affc8fb5d71e901d9630ffce66d
yyvf <yyvf@pm.me>
2025-04-04 12:35:37 -0300

    Update home/.config/nvim/lua/custom/plugins/lspconfig.lua Update home/.config/nvim/lua/lazy-plugins.lua

--- home/private_dot_config/nvim/lua/custom/plugins/lspconfig.lua
+++ home/private_dot_config/nvim/lua/custom/plugins/lspconfig.lua
@@ -1,118 +1,113 @@
 -- LSP Plugins
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "stevearc/conform.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim",
-    },
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "stevearc/conform.nvim",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "j-hui/fidget.nvim",
+  },
 
-    config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-            }
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+      }
+    })
+    local cmp = require('cmp')
+    local cmp_lsp = require("cmp_nvim_lsp")
+    local capabilities = vim.tbl_deep_extend(
+      "force",
+      {},
+      vim.lsp.protocol.make_client_capabilities(),
+      cmp_lsp.default_capabilities())
+
+    require("fidget").setup({})
+
+    -- Remove Mason setup
+    -- require("mason").setup()
+    -- require("mason-lspconfig").setup({...})
+
+    -- Setup for system-installed language servers
+    local lspconfig = require("lspconfig")
+
+    -- Lua
+    lspconfig.lua_ls.setup {
+      capabilities = capabilities,
+      settings = {
+        Lua = {
+          runtime = { version = "Lua 5.1" },
+          diagnostics = {
+            globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+          }
+        }
+      }
+    }
+
+    -- Rust
+    lspconfig.rust_analyzer.setup {
+      capabilities = capabilities,
+    }
+
+    -- Go
+    lspconfig.gopls.setup {
+      capabilities = capabilities,
+    }
+
+    -- Python
+    lspconfig.pyright.setup {
+      capabilities = capabilities,
+    }
+
+    -- C/C++
+    lspconfig.clangd.setup {
+      capabilities = capabilities,
+    }
+
+    -- Zig (if you have it installed)
+    lspconfig.zls.setup {
+      capabilities = capabilities,
+      root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
+      settings = {
+        zls = {
+          enable_inlay_hints = true,
+          enable_snippets = true,
+          warn_style = true,
+        },
+      },
+    }
+
+    local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+      }, {
+          { name = 'buffer' },
         })
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
+    })
 
-        require("fidget").setup({})
-
-        -- Remove Mason setup
-        -- require("mason").setup()
-        -- require("mason-lspconfig").setup({...})
-
-        -- Setup for system-installed language servers
-        local lspconfig = require("lspconfig")
-
-        -- Lua
-        lspconfig.lua_ls.setup {
-            capabilities = capabilities,
-            settings = {
-                Lua = {
-                    runtime = { version = "Lua 5.1" },
-                    diagnostics = {
-                        globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                    }
-                }
-            }
-        }
-
-        -- Rust
-        lspconfig.rust_analyzer.setup {
-            capabilities = capabilities,
-        }
-
-        -- Go
-        lspconfig.gopls.setup {
-            capabilities = capabilities,
-        }
-
-        -- Python
-        lspconfig.pyright.setup {
-            capabilities = capabilities,
-        }
-
-        -- C/C++
-        lspconfig.clangd.setup {
-            capabilities = capabilities,
-        }
-
-        -- Zig (if you have it installed)
-        lspconfig.zls.setup {
-            capabilities = capabilities,
-            root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-            settings = {
-                zls = {
-                    enable_inlay_hints = true,
-                    enable_snippets = true,
-                    warn_style = true,
-                },
-            },
-        }
-
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body)
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' },
-            }, {
-                { name = 'buffer' },
-            })
-        })
-
-        vim.diagnostic.config({
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
-            },
-        })
-    end
+    vim.diagnostic.config({
+      virtual_text = {
+        current_line = true
+      }
+    })
+  end
 }
 -- vim: ts=2 sts=2 sw=2 et
--- home/private_dot_config/nvim/lua/lazy-plugins.lua
+++ home/private_dot_config/nvim/lua/lazy-plugins.lua
@@ -25,27 +25,18 @@ require('lazy').setup({
   -- include a plugin definition from file lua/path/name.lua
 
   require 'kickstart/plugins/gitsigns',
-
   require 'kickstart/plugins/which-key',
-
   require 'kickstart/plugins/telescope',
+  require 'kickstart/plugins/conform',
+  require 'kickstart/plugins/cmp',
+  --require 'kickstart/plugins/tokyonight',
+  require 'kickstart/plugins/sonokai',
+  require 'kickstart/plugins/todo-comments',
+  --require 'kickstart/plugins/mini',
+  require 'kickstart/plugins/treesitter',
 
   require 'custom/plugins/oil',
   require 'custom/plugins/lspconfig',
-
-
-  require 'kickstart/plugins/conform',
-
-  require 'kickstart/plugins/cmp',
-
-  --require 'kickstart/plugins/tokyonight',
-  require 'kickstart/plugins/sonokai',
-
-  require 'kickstart/plugins/todo-comments',
-
-  --require 'kickstart/plugins/mini',
-
-  require 'kickstart/plugins/treesitter',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
