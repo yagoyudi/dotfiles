d41228e66515a6cca397585ecd21a923acecaee7
yyvf <yyvf@pm.me>
2025-07-02 21:53:38 -0300

    Update home/.config/nvim/lua/plugins/sonokai.lua

--- home/private_dot_config/nvim/lua/plugins/sonokai.lua
+++ home/private_dot_config/nvim/lua/plugins/sonokai.lua
@@ -1,17 +1,17 @@
 -- Colorscheme
 
 return {
-  {
-    'sainnhe/sonokai',
-    -- Make sure to load this before all the other start plugins:
-    priority = 1000,
-    init = function()
-      vim.cmd.colorscheme 'sonokai'
-    end,
+	{
+		'sainnhe/sonokai',
+		-- Make sure to load this before all the other start plugins:
+		priority = 1000,
+		init = function()
+			vim.cmd.colorscheme 'sonokai'
+		end,
 
-    config = function()
-      vim.g.sonokai_enable_italic = true
-      vim.g.sonokai_better_performance = 1
-    end,
-  },
+		config = function()
+			vim.g.sonokai_enable_italic = true
+			vim.g.sonokai_better_performance = 1
+		end,
+	},
 }
