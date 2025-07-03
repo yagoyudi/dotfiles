1fdffc9482e3f8934bb994693779a90391b99785
yyvf <yyvf@pm.me>
2025-07-02 21:52:24 -0300

    Update home/.config/nvim/lua/plugins/debug.lua Update home/.config/nvim/lua/plugins/guess-indent.lua Update home/.config/nvim/lua/plugins/indent-line.lua Update home/.config/nvim/lua/plugins/lint.lua Remove home/.config/nvim/lua/plugins/neo-tree.lua Update home/.config/nvim/lua/plugins/sonokai.lua

--- home/private_dot_config/nvim/lua/plugins/guess-indent.lua
+++ home/private_dot_config/nvim/lua/plugins/guess-indent.lua
@@ -2,6 +2,6 @@
 
 return {
 	{
-		'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+		'NMAC427/guess-indent.nvim',
 	},
 }
--- home/private_dot_config/nvim/lua/plugins/sonokai.lua
+++ home/private_dot_config/nvim/lua/plugins/sonokai.lua
@@ -1,11 +1,10 @@
+-- Colorscheme
+
 return {
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  {
     'sainnhe/sonokai',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    -- Make sure to load this before all the other start plugins:
+    priority = 1000,
     init = function()
       vim.cmd.colorscheme 'sonokai'
     end,
@@ -16,4 +15,3 @@ return {
     end,
   },
 }
--- vim: ts=2 sts=2 sw=2 et
