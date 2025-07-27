689c3ed6a346b33dea2ebe6db85ae363110da96f
yyvf <yyvf@pm.me>
2025-07-27 10:30:53 -0300

    Update home/.config/nvim/lua/keymaps.lua Update home/.config/nvim/lua/plugins/mini.lua

--- home/private_dot_config/nvim/lua/plugins/mini.lua
+++ home/private_dot_config/nvim/lua/plugins/mini.lua
@@ -19,6 +19,8 @@ return {
 			-- - sr)'  - [S]urround [R]eplace [)] [']
 			require('mini.surround').setup()
 
+			require('mini.pick').setup()
+
 			-- Simple and easy statusline.
 			--  You could remove this setup call if you don't like it,
 			--  and try some other statusline plugin
