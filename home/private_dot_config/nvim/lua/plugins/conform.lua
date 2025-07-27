c95c874d7592237b0c84ea2ea85fb39be61613c5
yyvf <yyvf@pm.me>
2025-07-27 12:12:10 -0300

    Update nvim

--- home/private_dot_config/nvim/lua/plugins/conform.lua
+++ home/private_dot_config/nvim/lua/plugins/conform.lua
@@ -26,11 +26,6 @@ return {
 				lua = { 'stylua' },
 				go = { 'goimports' },
 				yaml = { 'yamlfmt' },
-				-- Conform can also run multiple formatters sequentially
-				-- python = { "isort", "black" },
-				--
-				-- You can use 'stop_after_first' to run the first available formatter from the list
-				-- javascript = { "prettierd", "prettier", stop_after_first = true },
 			},
 		},
 	},
--- home/private_dot_config/nvim/lua/plugins/treesitter.lua
+++ home/private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -19,11 +19,5 @@ return {
 			},
 			indent = { enable = true, disable = { 'ruby' } },
 		},
-		-- There are additional nvim-treesitter modules that you can use to interact
-		-- with nvim-treesitter. You should go explore a few and see what interests you:
-		--
-		--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 	},
 }
