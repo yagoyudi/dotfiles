6f1564a7138fc129215efcc63390bbd8203521a7
yyvf <yyvf@pm.me>
2025-07-27 11:22:57 -0300

    Update home/.config/nvim/lua/keymaps.lua Remove home/.config/nvim/lua/plugins/debug.lua

--- home/private_dot_config/nvim/lua/keymaps.lua
+++ home/private_dot_config/nvim/lua/keymaps.lua
@@ -1,29 +1,21 @@
---  See `:help vim.keymap.set()`
-
--- Clear highlights on search when pressing <Esc> in normal mode
--- See `:help hlsearch`
+-- Clear highlights on search when pressing <Esc> in normal mode:
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
--- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit
 -- easier for people to discover. Otherwise, you normally need to press
 -- <C-\><C-n>, which is not what someone will guess without a bit more
--- experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own
--- mapping or just use <C-\><C-n> to exit terminal mode
+-- experience:
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- Keybinds to make split navigation easier. Use CTRL+<hjkl> to switch between
--- windows
---
---  See `:help wincmd` for a list of all window commands
+-- Keybinds to make split navigation easier:
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Pick:
+vim.keymap.set('n', '<leader>f', ':Pick files<CR>')
+vim.keymap.set('n', '<leader>h', ':Pick help<CR>')
 
 ---------------------------------------------------------------------------------------
 
@@ -38,7 +30,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 		vim.hl.on_yank()
 	end,
 })
-
-vim.keymap.set('n', '<leader>f', ':Pick files<CR>')
-vim.keymap.set('n', '<leader>h', ':Pick help<CR>')
-
