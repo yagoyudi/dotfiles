3176cf9e0e382e88c80cb62959a52edb4adbfed7
yyvf <yyvf@pm.me>
2025-07-27 17:37:53 -0300

    Add home/.config/zsh/aliases.zsh Add home/.config/zsh/environment.zsh Add home/.config/zsh/functions.zsh Add home/.config/zsh/keybinds.zsh Add home/.config/zsh/main.zsh Remove home/.bash_profile Remove home/.bashrc Remove home/.inputrc Change attributes of home/.zprofile

---
+++ home/private_dot_config/zsh/functions.zsh
@@ -0,0 +1,37 @@
+with_netbox() {
+  export NETBOX_TOKEN=$(gopass NETBOX_TOKEN)
+  export NETBOX_URL="https://netbox.c3sl.ufpr.br"
+
+  if [ "$#" -gt 0 ]; then
+    "$@"
+  fi
+}
+
+with_gitlab() {
+  export GITLAB_USERNAME=yyvf
+  export GITLAB_ACCESS_TOKEN=$(gopass GITLAB_TOKEN)
+
+  if [ "$#" -gt 0 ]; then
+    "$@"
+  fi
+}
+
+with_netbox_gitlab() {
+  with_netbox
+  with_gitlab
+
+  "$@"
+}
+
+# https://codeberg.org/dnkl/foot/wiki#user-content-spawning-new-terminal-instances-in-the-current-working-directory
+function osc7-pwd() {
+    emulate -L zsh # also sets localoptions for us
+    setopt extendedglob
+    local LC_ALL=C
+    printf '\e]7;file://%s%s\e\' $HOST ${PWD//(#m)([^@-Za-z&-;_~])/%${(l:2::0:)$(([##16]#MATCH))}}
+}
+
+function chpwd-osc7-pwd() {
+    (( ZSH_SUBSHELL )) || osc7-pwd
+}
+add-zsh-hook -Uz chpwd chpwd-osc7-pwd
---
+++ home/private_dot_config/zsh/main.zsh
@@ -0,0 +1,11 @@
+eval "$(starship init zsh)"
+eval "$(zoxide init zsh)"
+
+source <(kubectl completion zsh)
+source <(chezmoi completion zsh)
+source <(gopass completion zsh)
+
+source ~/.config/zsh/environment.zsh
+source ~/.config/zsh/aliases.zsh
+source ~/.config/zsh/functions.zsh
+source ~/.config/zsh/keybinds.zsh
