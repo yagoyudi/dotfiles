d29cec0eb037a87c3d3a91b76b088347439bd766
yyvf <yyvf@pm.me>
2025-07-30 12:17:30 -0300

    feat(zsh): vi mode plugin

--- nixos/modules/zsh.nix
+++ nixos/modules/zsh.nix
@@ -2,10 +2,17 @@
 {
 	users.defaultUserShell = pkgs.zsh;
 
+	environment.systemPackages = with pkgs; [
+		zsh-vi-mode
+	];
+
 	programs.zsh = {
 		enable = true;
 		enableCompletion = true;
 		autosuggestions.enable = true;
 		syntaxHighlighting.enable = true;
+		interactiveShellInit = ''
+			source ${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
+		'';
 	};
 }
