0f60fbd16b72f3a82b3555deb718841d94c6887f
yyvf <yyvf@pm.me>
2025-08-23 08:30:05 -0300

    Update .config/bash/environment.bash Update .config/bash/main.bash

--- home/private_dot_config/bash/main.bash
+++ home/private_dot_config/bash/main.bash
@@ -4,12 +4,12 @@ shopt -s autocd
 eval "$(starship init bash)"
 eval "$(zoxide init bash)"
 
-source <(kubectl completion bash)
-source <(chezmoi completion bash)
-source <(gopass completion bash)
+# source <(kubectl completion bash)
+# source <(chezmoi completion bash)
+# source <(gopass completion bash)
 
-complete -F __start_kubectl k
-complete -F __start_chezmoi c
+# complete -F __start_kubectl k
+# complete -F __start_chezmoi c
 
 source ~/.config/bash/environment.bash
 source ~/.config/bash/aliases.bash
