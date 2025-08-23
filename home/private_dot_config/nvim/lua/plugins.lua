a2f0f46e6dc914e1caaae5e3936f91a4e6fe06f2
yyvf <yyvf@pm.me>
2025-08-23 19:30:00 -0300

    Update .config/nvim/lua/plugins.lua

--- home/private_dot_config/nvim/lua/plugins.lua
+++ home/private_dot_config/nvim/lua/plugins.lua
@@ -52,72 +52,63 @@ require('mini.ai').setup({
 -- Surround:
 require('mini.surround').setup()
 
--- Statusline:
-local statusline = require('mini.statusline')
-statusline.setup({
-	use_icons = false,
-})
-statusline.section_location = function()
-	return '%2l:%-2v'
-end
-
 -- LSP:
-vim.lsp.enable({
-	'gopls',
-	'lua_ls',
-	'yamlls',
-	'marksman',
-})
+-- vim.lsp.enable({
+-- 	'gopls',
+-- 	'lua_ls',
+-- 	'yamlls',
+-- 	'marksman',
+-- })
 
 -- Golang LSP config:
-vim.lsp.config('gopls', {
-	settings = {
-		gopls = {
-			analyses ={
-				unusedparams = true,
-			},
-			staticcheck = true,
-			gofumpt = true,
-		}
-	}
-})
-
-vim.lsp.config('lua_ls', {
-	settings = {
-		Lua = {
-			diagnostics = {
-				-- Make lua_ls recognize the vim global:
-				globals = {
-					'vim',
-				},
-			},
-		},
-	},
-})
+-- vim.lsp.config('gopls', {
+-- 	settings = {
+-- 		gopls = {
+-- 			analyses ={
+-- 				unusedparams = true,
+-- 			},
+-- 			staticcheck = true,
+-- 			gofumpt = true,
+-- 		}
+-- 	}
+-- })
+--
+-- vim.lsp.config('lua_ls', {
+-- 	settings = {
+-- 		Lua = {
+-- 			diagnostics = {
+-- 				-- Make lua_ls recognize the vim global:
+-- 				globals = {
+-- 					'vim',
+-- 				},
+-- 			},
+-- 		},
+-- 	},
+-- })
 
 -- Golang import and format:
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*.go",
-	callback = function()
-		local params = vim.lsp.util.make_range_params()
-		params.context = {only = {"source.organizeImports"}}
-		-- buf_request_sync defaults to a 1000ms timeout. Depending on your
-		-- machine and codebase, you may want longer. Add an additional
-		-- argument after params if you find that you have to write the file
-		-- twice for changes to be saved.
-		-- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
-		local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-		for cid, res in pairs(result or {}) do
-			for _, r in pairs(res.result or {}) do
-				if r.edit then
-					local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-					vim.lsp.util.apply_workspace_edit(r.edit, enc)
-				end
-			end
-		end
-		vim.lsp.buf.format({async = false})
-	end
-})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+-- 	pattern = "*.go",
+-- 	callback = function()
+-- 		local params = vim.lsp.util.make_range_params()
+-- 		params.context = {only = {"source.organizeImports"}}
+-- 		-- buf_request_sync defaults to a 1000ms timeout. Depending on your
+-- 		-- machine and codebase, you may want longer. Add an additional
+-- 		-- argument after params if you find that you have to write the file
+-- 		-- twice for changes to be saved.
+-- 		-- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+-- 		local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+-- 		for cid, res in pairs(result or {}) do
+-- 			for _, r in pairs(res.result or {}) do
+-- 				if r.edit then
+-- 					local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+-- 					vim.lsp.util.apply_workspace_edit(r.edit, enc)
+-- 				end
+-- 			end
+-- 		end
+-- 		vim.lsp.buf.format({async = false})
+-- 	end
+-- })
 
 -- LSP Autocompletion:
 vim.api.nvim_create_autocmd('LspAttach', {
