b32fbc60d1610653cdfa84cfbe630cd79581e776
yyvf <yyvf@pm.me>
2025-08-23 08:47:58 -0300

    Update .config/bash/environment.bash

--- home/private_dot_config/bash/environment.bash
+++ home/private_dot_config/bash/environment.bash
@@ -5,4 +5,3 @@ export PASSWORD_STORE_DIR="$HOME/github.com/yyvfx/pass"
 export STARSHIP_CONFIG=$HOME/.config/starship/config.toml
 export TZ="America/Sao_Paulo"
 export GIT_PAGER="cat"
-export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent.socket
