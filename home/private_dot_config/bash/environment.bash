0dad03f835b666e253a72e1a7e3fb113d34e55b9
yyvf <yyvf@pm.me>
2025-08-24 18:34:13 -0300

    Update .config/bash/environment.bash

--- home/private_dot_config/bash/environment.bash
+++ home/private_dot_config/bash/environment.bash
@@ -1,6 +1,6 @@
 export EDITOR="nvim"
 export GOPATH="$HOME/.local/go"
-export PATH="$PATH:$GOPATH/bin:$HOME/.local/bin"
+export PATH="$PATH:$GOPATH/bin:$HOME/.local/bin:$HOME/.nix-profile/bin"
 export PASSWORD_STORE_DIR="$HOME/github.com/yyvfx/pass"
 export STARSHIP_CONFIG=$HOME/.config/starship/config.toml
 export TZ="America/Sao_Paulo"
